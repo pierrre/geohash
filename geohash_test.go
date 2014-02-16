f12c8af0aec4f970f69a6ffcadb3a262cd6330d6
Pierre Durand <pierredurand@gmail.com>
2014-02-16 02:54:32 +0100

    add benchmark comparison

--- geohash_test.go
+++ geohash_test.go
@@ -1,6 +1,10 @@
 package geohash
 
 import (
+	codefor_geohash "github.com/Codefor/geohash"
+	broady_geohash "github.com/broady/gogeohash"
+	gnagel_geohash "github.com/gnagel/go-geohash/ggeohash"
+	the42_cartconvert_geohash "github.com/the42/cartconvert/cartconvert"
 	"testing"
 )
 
@@ -43,5 +47,61 @@ func BenchmarkEncode(b *testing.B) {
 func BenchmarkDecode(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Decode(testGeohash)
+	}
+}
+
+func BenchmarkBroadyEncode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		broady_geohash.Encode(testPoint.Lat, testPoint.Lon)
+	}
+}
+
+func BenchmarkBroadyDecode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		broady_geohash.Decode(testGeohash)
+	}
+}
+
+func BenchmarkGnagelEncode(b *testing.B) {
+	precision := uint8(testPrecision)
+	for i := 0; i < b.N; i++ {
+		gnagel_geohash.Encode(testPoint.Lat, testPoint.Lon, precision)
+	}
+}
+
+func BenchmarkGnagelDecode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		gnagel_geohash.DecodeBoundBox(testGeohash)
+	}
+}
+
+func BenchmarkThe42CartconvertEncode(b *testing.B) {
+	pc := &the42_cartconvert_geohash.PolarCoord{
+		Latitude:  testPoint.Lat,
+		Longitude: testPoint.Lon,
+		Height:    0,
+		El:        the42_cartconvert_geohash.DefaultEllipsoid,
+	}
+	precision := byte(testPrecision)
+	for i := 0; i < b.N; i++ {
+		the42_cartconvert_geohash.LatLongToGeoHashBits(pc, precision)
+	}
+}
+
+func BenchmarkThe42CartconvertDecode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		the42_cartconvert_geohash.GeoHashToLatLong(testGeohash, the42_cartconvert_geohash.DefaultEllipsoid)
+	}
+}
+
+func BenchmarkCodeforEncode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		codefor_geohash.Encode(testPoint.Lat, testPoint.Lon)
+	}
+}
+
+func BenchmarkCodeforDecode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		codefor_geohash.Decode(testGeohash)
 	}
 }
