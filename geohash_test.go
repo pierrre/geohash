bc48d06b08e1524bb4918f385774ec6ccd00dc65
Pierre Durand <pierredurand@gmail.com>
2014-02-17 23:45:50 +0100

    more tests

--- geohash_test.go
+++ geohash_test.go
@@ -40,7 +40,7 @@ func TestDecode(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !(pointIsInsideBox(testPoint, box)) {
+	if !(testPointIsInsideBox(testPoint, box)) {
 		t.Fatal("point is outside")
 	}
 }
@@ -49,6 +49,36 @@ func TestDecodeInvalidCharacter(t *testing.T) {
 	_, err := Decode("é")
 	if err == nil {
 		t.Fatal("no error")
+	}
+}
+
+func TestBoxCenter(t *testing.T) {
+	box := Box{
+		Lat: Range{
+			Min: testPoint.Lat - 1,
+			Max: testPoint.Lat + 1,
+		},
+		Lon: Range{
+			Min: testPoint.Lon - 1,
+			Max: testPoint.Lon + 1,
+		},
+	}
+	if box.Center() != testPoint {
+		t.Fatal("invalid center")
+	}
+}
+
+func TestBoxRound(t *testing.T) {
+	box, err := Decode(testGeohash)
+	if err != nil {
+		t.Fatal(err)
+	}
+	round := box.Round()
+	if round != testPoint {
+		t.Fatal("invalid round")
+	}
+	if round == box.Center() {
+		t.Fatal("round is equal to center")
 	}
 }
 
@@ -129,11 +159,11 @@ func BenchmarkBroadyDecode(b *testing.B) {
 	}
 }
 
-func pointIsInsideBox(p Point, b Box) bool {
-	return valueIsInsideRange(p.Lat, b.Lat) &&
-		valueIsInsideRange(p.Lon, b.Lon)
+func testPointIsInsideBox(p Point, b Box) bool {
+	return testValueIsInsideRange(p.Lat, b.Lat) &&
+		testValueIsInsideRange(p.Lon, b.Lon)
 }
 
-func valueIsInsideRange(v float64, r Range) bool {
+func testValueIsInsideRange(v float64, r Range) bool {
 	return v >= r.Min && v <= r.Max
 }
