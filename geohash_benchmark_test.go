48373956228b30cb2b0e898c4adb37250cf4a01c
Pierre Durand <pierredurand@gmail.com>
2016-01-13 19:31:18 +0100

    use classic benchmark instead of parallel

--- geohash_benchmark_test.go
+++ geohash_benchmark_test.go
@@ -12,131 +12,99 @@ import (
 )
 
 func BenchmarkEncode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			Encode(testPoint.Lat, testPoint.Lon, testPrecision)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		Encode(testPoint.Lat, testPoint.Lon, testPrecision)
+	}
 }
 
 func BenchmarkDecode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			Decode(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		Decode(testGeohash)
+	}
 }
 
 func BenchmarkNeighbors(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			GetNeighbors(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		GetNeighbors(testGeohash)
+	}
 }
 
 func BenchmarkCodeforEncode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			codefor_geohash.Encode(testPoint.Lat, testPoint.Lon)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		codefor_geohash.Encode(testPoint.Lat, testPoint.Lon)
+	}
 }
 
 func BenchmarkCodeforDecode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			codefor_geohash.Decode(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		codefor_geohash.Decode(testGeohash)
+	}
 }
 
 func BenchmarkTomiHiltunenEncode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			tomi_hiltunen_geohash.EncodeWithPrecision(testPoint.Lat, testPoint.Lon, testPrecision)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		tomi_hiltunen_geohash.EncodeWithPrecision(testPoint.Lat, testPoint.Lon, testPrecision)
+	}
 }
 
 func BenchmarkTomiHiltunenDecode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			tomi_hiltunen_geohash.Decode(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		tomi_hiltunen_geohash.Decode(testGeohash)
+	}
 }
 
 func BenchmarkTomiHiltunenNeighbors(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			tomi_hiltunen_geohash.CalculateAllAdjacent(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		tomi_hiltunen_geohash.CalculateAllAdjacent(testGeohash)
+	}
 }
 
 func BenchmarkBroadyEncode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			broady_geohash.Encode(testPoint.Lat, testPoint.Lon)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		broady_geohash.Encode(testPoint.Lat, testPoint.Lon)
+	}
 }
 
 func BenchmarkBroadyDecode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			broady_geohash.Decode(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		broady_geohash.Decode(testGeohash)
+	}
 }
 
 func BenchmarkFanixkEncode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			fanixk_geohash.PrecisionEncode(testPoint.Lat, testPoint.Lon, testPrecision)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		fanixk_geohash.PrecisionEncode(testPoint.Lat, testPoint.Lon, testPrecision)
+	}
 }
 
 func BenchmarkFanixkDecode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			fanixk_geohash.DecodeBoundingBox(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		fanixk_geohash.DecodeBoundingBox(testGeohash)
+	}
 }
 
 func BenchmarkFanixkNeighbors(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			fanixk_geohash.Neighbors(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		fanixk_geohash.Neighbors(testGeohash)
+	}
 }
 
 func BenchmarkMmcloughlinEncode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			mmcloughlin_geohash.EncodeWithPrecision(testPoint.Lat, testPoint.Lon, testPrecision)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		mmcloughlin_geohash.EncodeWithPrecision(testPoint.Lat, testPoint.Lon, testPrecision)
+	}
 }
 
 func BenchmarkMmcloughlinDecode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			mmcloughlin_geohash.BoundingBox(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		mmcloughlin_geohash.BoundingBox(testGeohash)
+	}
 }
 
 func BenchmarkMmcloughlinNeighbors(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			fanixk_geohash.Neighbors(testGeohash)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		fanixk_geohash.Neighbors(testGeohash)
+	}
 }
 
 func BenchmarkThe42CartconvertEncode(b *testing.B) {
@@ -147,17 +115,13 @@ func BenchmarkThe42CartconvertEncode(b *testing.B) {
 		El:        the42_cartconvert_geohash.DefaultEllipsoid,
 	}
 	precision := byte(testPrecision)
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			the42_cartconvert_geohash.LatLongToGeoHashBits(pc, precision)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		the42_cartconvert_geohash.LatLongToGeoHashBits(pc, precision)
+	}
 }
 
 func BenchmarkThe42CartconvertDecode(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			the42_cartconvert_geohash.GeoHashToLatLong(testGeohash, the42_cartconvert_geohash.DefaultEllipsoid)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		the42_cartconvert_geohash.GeoHashToLatLong(testGeohash, the42_cartconvert_geohash.DefaultEllipsoid)
+	}
 }
