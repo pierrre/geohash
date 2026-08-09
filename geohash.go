eab8fba196adfb208d6c234bfc2536794ccd0aa1
Pierre Durand <pierredurand@gmail.com>
2026-08-09 05:15:48 +0200

    refactor: use min/max builtins for precision clamping in Encode

--- geohash.go
+++ geohash.go
@@ -50,12 +50,7 @@ const encodeMaxPrecision = 32
 //
 // The maximum supported precision is 32.
 func Encode(lat, lon float64, precision int) string {
-	if precision < 0 {
-		precision = 0
-	}
-	if precision > encodeMaxPrecision {
-		precision = encodeMaxPrecision
-	}
+	precision = min(max(precision, 0), encodeMaxPrecision)
 	var buf [encodeMaxPrecision]byte
 	box := defaultBox
 	even := true
